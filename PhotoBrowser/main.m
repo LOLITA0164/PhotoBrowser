7300f2c8f8f781c1cf11b7de62ab3342b1fcc679
LOLITA <476512340@qq.com>
2017-09-05 12:51:51 +0800

    Initial Commit

---
+++ PhotoBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoBrowser
+//
+//  Created by LOLITA on 2017/9/5.
+//  Copyright © 2017年 LOLITA0164. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
